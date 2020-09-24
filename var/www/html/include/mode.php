45ac69869f31cfb68c930541f05c3b43510ac15d
Waldek <sp2ong@gmail.com>
2020-09-24 09:49:47 +0200

    Update mode.php
    update show last TG

--- var/www/html/include/mode.php
+++ var/www/html/include/mode.php
@@ -94,7 +94,8 @@ if (file_exists('/var/www/html/ABInfo.json')) {
     echo "<tr><th>GW ID</th><td style=\"background: #ffffff;\">".$abinfo['digital']['gw']."</td></tr>\n";
     echo "<tr><th>RPT ID</th><td style=\"background: #ffffff;\">".$abinfo['digital']['rpt']."</td></tr>\n";
     echo "<tr><th>Mode</th><td style=\"background: #ffffff;font-weight: bold;color:brown;\">".$abinfo['tlv']['ambe_mode']."</td></tr>\n";
-    echo "<tr><th>Last TG</th><td style=\"background: #ffffff;font-weight: bold;color:brown;\">".$abinfo['last_tune']."</td></tr>\n";
+    if ($abinfo['last_tune']=="") {$tginfo=$abinfo['digital']['tg'];} else {$tginfo=$abinfo['last_tune']; }
+    echo "<tr><th>Last TG</th><td style=\"background: #ffffff;font-weight: bold;color:brown;\">".$tginfo."</td></tr>\n";
     echo "<tr><th>AB ver</th><td style=\"background: #ffffff;\">".$abinfo['ab']['version']."</td></tr>\n";
     echo "</table>\n";
 }
