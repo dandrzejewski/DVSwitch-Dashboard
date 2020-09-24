be994b6870727aec27fed8c8e6084147a3a64e1c
Waldek <sp2ong@gmail.com>
2020-09-24 13:02:59 +0200

    Update functions.php
    rename mode.php to status.php

--- var/www/html/include/functions.php
+++ var/www/html/include/functions.php
@@ -1000,7 +1000,7 @@ if (!in_array($_SERVER["PHP_SELF"],array('/include/bm_links.php','/include/bm_ma
 	$lastHeard = getLastHeard($reverseLogLinesMMDVM);
 
 	// Only need these in repeaterinfo.php
-	if (strpos($_SERVER["PHP_SELF"], 'mode.php') !== false || strpos($_SERVER["PHP_SELF"], 'index.php') !== false) {
+	if (strpos($_SERVER["PHP_SELF"], 'status.php') !== false || strpos($_SERVER["PHP_SELF"], 'stystem.php') !== false || strpos($_SERVER["PHP_SELF"], 'index.php') !== false) {
 		//$YSFGatewayconfigs = getYSFGatewayConfig();
 		$logLinesYSFGateway = getYSFGatewayLog();
 		$reverseLogLinesYSFGateway = $logLinesYSFGateway;
