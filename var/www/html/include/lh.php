b2fb127adc067bf22d36b7aa5f494e230746989b
Waldek <sp2ong@gmail.com>
2020-09-29 11:30:34 +0200

    Update lh.php
    add display info about GPS

--- var/www/html/include/lh.php
+++ var/www/html/include/lh.php
@@ -61,6 +61,8 @@ for ($i = 0;  ($i <= 19); $i++) { //Last 20 calls
 				echo "<td colspan=\"3\" style=\"background:#f33;\">TX</td>";
 			} else if ($listElem[6] == "SMS") {
 				echo "<td colspan=\"3\" style=\"background:#1d1;\">SMS</td>";
+			} else if ($listElem[6] == "GPS") {
+				echo "<td colspan=\"3\" style=\"background:#1d1;\">GPS</td>";
 			} else {
 			echo "<td>$listElem[6]</td>";
 
