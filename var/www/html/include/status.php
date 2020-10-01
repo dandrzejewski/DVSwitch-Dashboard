7c64cf58a83586f28ea8c71d03e7deba2c91d40f
Waldek <sp2ong@gmail.com>
2020-10-01 16:23:56 +0200

    Update status.php

--- var/www/html/include/status.php
+++ var/www/html/include/status.php
@@ -5,7 +5,7 @@ include_once $_SERVER['DOCUMENT_ROOT'].'/include/functions.php';
 
 ?>
 <span style="font-weight: bold;font-size:14px;">Status</span>
-<fieldset style="background-color:#e8e8e8e8;width:160px;margin-top:4px;;margin-bottom:0px;margin-left:0px;margin-right:3px;font-size:12px;border-top-left-radius: 10px; border-top-right-radius: 10px;border-bottom-left-radius: 10px; border-bottom-right-radius: 10px;">
+<fieldset style="background-color:#e8e8e8e8;width:160px;margin-top:6px;;margin-bottom:0px;margin-left:0px;margin-right:3px;font-size:12px;border-top-left-radius: 10px; border-top-right-radius: 10px;border-bottom-left-radius: 10px; border-bottom-right-radius: 10px;">
 <?php
 $testMMDVModeDMR = getConfigItem("DMR", "Enable", $mmdvmconfigs);
 if ( $testMMDVModeDMR == 1 ) { //Hide the DMR information when DMR mode not enabled.
