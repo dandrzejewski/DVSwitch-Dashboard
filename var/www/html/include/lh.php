6cbf0d0a2bb528c45dc82ba285825aceb68e8531
Waldek <sp2ong@gmail.com>
2020-10-04 19:32:40 +0200

    Update lh.php

--- var/www/html/include/lh.php
+++ var/www/html/include/lh.php
@@ -35,7 +35,7 @@ for ($i = 0;  ($i <= 19); $i++) { //Last 20 calls
 		if (is_numeric($listElem[2]) || strpos($listElem[2], "openSPOT") !== FALSE || $listElem[2] == "N0CALL") {
 		    echo "<td align=\"left\"><b>&nbsp;$listElem[2]</b></td>";
 		} elseif (!preg_match('/[A-Za-z].*[0-9]|[0-9].*[A-Za-z]/', $listElem[2])) {
- 	                       echo "<td align=\"left\">&nbsp;$listElem[2]</td>";
+ 	                       echo "<td align=\"left\"><b>&nbsp;$listElem[2]</b></td>";
 		} else {
 		    if (strpos($listElem[2],"-") > 0) { $listElem[2] = substr($listElem[2], 0, strpos($listElem[2],"-")); }
 			    if ( $listElem[3] && $listElem[3] != '    ' ) {
