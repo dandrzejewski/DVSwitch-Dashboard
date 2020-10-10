f4cd7a0fdf736d04d9b4fe858c6ef6c2018e4390
Waldek <sp2ong@gmail.com>
2020-10-10 14:45:17 +0200

    added  REMOTENET

--- var/www/html/include/config.php
+++ var/www/html/include/config.php
@@ -4,6 +4,9 @@ error_reporting(E_ALL & ~E_NOTICE);
 
 // ABINFO = rxPort from [USRP] Analog_Bridge.ini
 define("ABINFO", "34001");
+// Put remote Network or IP address from which want to see tooltip ABInfo
+// IP address has mast /32 for netoworks use /24 etc 
+define("REMOTENET", "127.0.0.1/32");
 define("LOGPATH", "/var/log/mmdvm");
 define("MMDVMLOGPREFIX", "MMDVM_Bridge");
 define("MMDVMINIPATH", "/opt/MMDVM_Bridge/");
